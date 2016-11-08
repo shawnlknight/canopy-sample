39b4fe9267e1127bd9a1186705a4125baea919a0
Travis Nesland <tnesland@gmail.com>
2016-11-08 10:45:11 -0500

    Add redux.
    We are using a `react-redux` for our interface work at Canopy. I want to
    see candidates use this stack whether they have had previous experience
    with it or not.

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+// import { combineReducers } from 'redux';
+
+// const rootReducer = combineReducers({
+// });
+
+// TODO: this is a temporary placeholder, this reducer simply returns whatever
+// state it was passed, which is valid but useless
+const rootReducer = (state = {}, action) => {
+  return state;
+};
+
+export default rootReducer;
