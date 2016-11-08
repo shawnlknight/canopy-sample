e26fb4d8531122daa96e3b67bfbe12cb940c8195
Travis Nesland <tnesland@gmail.com>
2016-11-08 17:11:34 -0500

    Add header component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import Comments from './containers/Comments';
+import Header from './components/Header';
 import ScheduledItems from './containers/ScheduledItems';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Canopy</h1>
+        <Header />
+        <div className="content-aligned">
 
-        <div>
-          <h2>Job Completed</h2>
-          <p>Your Full Service Deluxe visit at 1234 Main Street has been completed on Monday, November 7th, 2016.</p>
+          <div>
+            <h2>Job Completed</h2>
+            <p>Your Full Service Deluxe visit at 1234 Main Street has been completed on Monday, November 7th, 2016.</p>
+          </div>
+
+          <ScheduledItems />
+
+          <div>Technician Component</div>
+
+          <div>photo component</div>
+
+          <Comments />
         </div>
-
-        <ScheduledItems />
-
-        <div>Technician Component</div>
-
-        <div>photo component</div>
-
-        <Comments />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="content-aligned">Canopy</h1>
+    </div>
+  );
+}
+
+export default Header;
