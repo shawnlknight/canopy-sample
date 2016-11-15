2a147763982a10fc105a77f245d3075f4f111bed
Travis Nesland <tnesland@gmail.com>
2016-11-15 08:53:17 -0500

    Add store provider to render App test.
    This was previously failing because no store was provided.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configure-store';
 import App from './App';
+
+const store = configureStore();
+
+const RootElement = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(RootElement, div);
 });
